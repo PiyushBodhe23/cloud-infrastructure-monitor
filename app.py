58ab89f325ecc5acf64b2d822ef870795dd608db
PiyushBodhe23 <piyushbodhe23@gmail.com>
2026-07-10 01:03:57 +0530

    mid

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from flask import Flask, jsonify, render_template
 from monitor import get_metrics
-from database import get_all_metrics, save_metrics
+from database import (
+    get_all_metrics,
+    save_metrics,
+    create_incident,
+    get_incidents
+)
 
 app = Flask(__name__)
 
@@ -8,8 +13,10 @@ app = Flask(__name__)
 def dashboard():
     return render_template("dashboard.html")
 
+
 @app.route("/metrics")
 def metrics():
+
     data = get_metrics()
 
     save_metrics(
@@ -18,11 +25,29 @@ def metrics():
         data["disk"]
     )
 
+    # Incident Detection
+
+    if data["cpu"] > 80:
+        create_incident("High CPU Usage")
+
+    if data["memory"] > 90:
+        create_incident("High Memory Usage")
+
+    if data["disk"] > 90:
+        create_incident("Disk Almost Full")
+
     return jsonify(data)
+
 
 @app.route("/history")
 def history():
     return jsonify(get_all_metrics())
+
+
+@app.route("/incidents")
+def incidents():
+    return jsonify(get_incidents())
+
 
 if __name__ == "__main__":
     app.run(debug=True)
--- database.py
+++ database.py
@@ -1,6 +1,8 @@
 import sqlite3
 
+
 def init_db():
+
     conn = sqlite3.connect("monitor.db")
 
     cursor = conn.cursor()
@@ -15,10 +17,20 @@ def init_db():
     )
     """)
 
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS incidents(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        message TEXT,
+        timestamp DATETIME DEFAULT CURRENT_TIMESTAMP
+    )
+    """)
+
     conn.commit()
     conn.close()
 
+
 def save_metrics(cpu, memory, disk):
+
     conn = sqlite3.connect("monitor.db")
 
     cursor = conn.cursor()
@@ -28,13 +40,33 @@ def save_metrics(cpu, memory, disk):
         INSERT INTO metrics(cpu,memory,disk)
         VALUES(?,?,?)
         """,
-        (cpu,memory,disk)
+        (cpu, memory, disk)
     )
 
     conn.commit()
     conn.close()
 
+
+def create_incident(message):
+
+    conn = sqlite3.connect("monitor.db")
+
+    cursor = conn.cursor()
+
+    cursor.execute(
+        """
+        INSERT INTO incidents(message)
+        VALUES(?)
+        """,
+        (message,)
+    )
+
+    conn.commit()
+    conn.close()
+
+
 def get_all_metrics():
+
     conn = sqlite3.connect("monitor.db")
 
     cursor = conn.cursor()
@@ -51,3 +83,26 @@ def get_all_metrics():
     conn.close()
 
     return rows
+
+
+def get_incidents():
+
+    conn = sqlite3.connect("monitor.db")
+
+    cursor = conn.cursor()
+
+    cursor.execute("""
+    SELECT *
+    FROM incidents
+    ORDER BY id DESC
+    LIMIT 20
+    """)
+
+    rows = cursor.fetchall()
+
+    conn.close()
+
+    return rows
+
+
+init_db()
